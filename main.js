63c639778c5bc6f7bf78d3bd8ec24f1ee7f8f2d8
Arpad Korossy <korossy.arpad@gmail.com>
2024-02-14 17:17:43 -0500

    Bugged San Diego Top Spots project

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-$(document).ready(function() {
-// write your code here
+$(document).ready(function(){
 
+$.getJSON("data.json", function(data){
+    console.log("Entered getJSON function")
+   let placeholder = document.querySelector("#output");
+   let out = "";
+   for(let spot of data){
+    console.log("One iteration of JSON for-of loop");
+      out += `
+         <tr>
+            <td>${spot.name}</td>
+            <td>${spot.description}</td>
+            <td><a href="https://www.google.com/maps?q=${spot.location}"><img src="open-in-google-maps.png"></a></td>
+         </tr>
+      `;
+   }
+ 
+   placeholder.innerHTML = out;
+});
 });
